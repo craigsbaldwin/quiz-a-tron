4fb0477379467f5de53e18d7d694ae6ddacf7855
Craig Baldwin <craigsbaldwin@gmail.com>
2019-12-20 15:31:19 +0000

    FEATURE: Added reset function to console

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
 window.VueEventBus = new Vue();
 
+/**
+ * Debugging tools.
+ */
+window.reset = function() {
+  localStorage.removeItem('quiz');
+}
+
+/**
+ * Vue instance.
+ */
 new Vue({
   render: h => h(App),
 }).$mount('#app');
