1fd6a2e98b8aa97b40b80852e7b00c9fd23fb6bd
Craig Baldwin <craig@wemakewebsites.com>
2020-01-06 15:55:17 +0000

    BUILD: Sorted results into position groups

--- src/components/ResultsTable.vue
+++ src/components/ResultsTable.vue
@@ -9,12 +9,39 @@
       v-if="unlocked"
       class="results-table"
     >
-      <div>
-        {{ results[show - 1].name }}
+      <div v-if="show === 1">
+        <Person
+          v-for="(person, index) in places.first"
+          :index="index"
+          :key="index"
+          :person="person"
+          place="1"
+        />
+      </div>
+
+      <div v-if="show === 2">
+        <Person
+          v-for="(person, index) in places.second"
+          :index="index"
+          :key="index"
+          :person="person"
+          place="2"
+        />
+      </div>
+
+      <div v-if="show === 3">
+        <Person
+          v-for="(person, index) in places.third"
+          :index="index"
+          :key="index"
+          :person="person"
+          place="3"
+        />
       </div>
 
       <button
         v-if="show !== 1"
+        class="button"
         type="button"
         @click="show -= 1"
       >
@@ -26,10 +53,12 @@
 
 <script>
   import PasswordForm from './PasswordForm.vue';
+  import Person from './Person.vue';
 
   export default {
     components: {
       PasswordForm,
+      Person,
     },
 
     props: {
@@ -40,7 +69,51 @@
     data() {
       return {
         show: 3,
+        tempResults: this.results,
+        places: {
+          first: [],
+          second: [],
+          third: [],
+        },
       }
     },
+
+    mounted() {
+      this.calculateWinners('first');
+      this.calculateWinners('second');
+      this.calculateWinners('third');
+    },
+
+    methods: {
+
+      /**
+       * Return winner array.
+       * @param {String} position - Position.
+       */
+      calculateWinners(position) {
+        this.places[position].push(this.tempResults[0]);
+        this.tempResults = this.arrayRemove(this.tempResults, 0);
+
+        this.tempResults.forEach((result, index) => {
+          if (result.score < this.places[position][0].score) {
+            return;
+          }
+
+          this.places[position].push(result);
+          this.tempResults = this.arrayRemove(this.tempResults, index);
+        });
+      },
+
+      /**
+       * Remove item from array.
+       * @param {Array} array - Array to filter.
+       * @param {Number} index - Index of item to remove.
+       */
+      arrayRemove(array, index) {
+        return array.filter((item, filterIndex) => {
+          return filterIndex !== index;
+        });
+      },
+    }
   }
 </script>
