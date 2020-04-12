d6515c8d356452716cfe9b8f34b564b016c111f8
Craig Baldwin <craigsbaldwin@gmail.com>
2020-04-12 14:21:19 +0100

    FEATURE: Added loading robot

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="loading">
-    <div class="loading__animation">
-      <div class="loading__square loading__square--1"></div>
-      <div class="loading__square loading__square--2"></div>
-      <div class="loading__square loading__square--3"></div>
-      <div class="loading__square loading__square--4"></div>
-      <div class="loading__square loading__square--5"></div>
-      <div class="loading__square loading__square--6"></div>
-      <div class="loading__square loading__square--7"></div>
-      <div class="loading__square loading__square--8"></div>
-      <div class="loading__square loading__square--9"></div>
+    <div class="loading__container">
+      <div class="loading__robot">
+        <div class="loading__animation">
+          <div class="loading__square loading__square--1"></div>
+          <div class="loading__square loading__square--2"></div>
+          <div class="loading__square loading__square--3"></div>
+          <div class="loading__square loading__square--4"></div>
+          <div class="loading__square loading__square--5"></div>
+          <div class="loading__square loading__square--6"></div>
+          <div class="loading__square loading__square--7"></div>
+          <div class="loading__square loading__square--8"></div>
+          <div class="loading__square loading__square--9"></div>
+        </div>
+
+        <img alt="Robot" src="../assets/robot.svg">
+      </div>
     </div>
 
     <div class="loading__text">{{ text }}</div>
