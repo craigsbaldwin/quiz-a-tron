145f3015b9deab1d9bc58d4b90a3f83be26e62bf
Craig Baldwin <craigsbaldwin@gmail.com>
2020-04-12 11:10:10 +0100

    BUILD: Added dynamic text to buttons disabled state

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -8,9 +8,8 @@
       <span
         class="button__text button__disabled"
         :class="{ 'is-hidden': !isDisabled }"
-      >
-        Please provide answers
-      </span>
+        v-text="renderDisabledButtonText()"
+      ></span>
 
       <span
         v-text="(step !== length) ? 'Next question' : 'Finish'"
@@ -40,6 +39,20 @@
 
         return answered.length > 0;
       }
-    }
+    },
+
+    methods: {
+
+      /**
+       * Determines button text based on number of choices.
+       */
+      renderDisabledButtonText() {
+        if (this.question.choices.length > 1) {
+          return 'Please provide answers';
+        }
+
+        return 'Please provide an answer';
+      },
+    },
   }
 </script>
